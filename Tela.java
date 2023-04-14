897eabef58ec45c5ca75cbf7d6f881cb1cfa2f00
RodolphoVSantoro <rodolphovs@gmail.com>
2023-04-13 21:40:04 -0300

    feat: add board drawing

--- Tela.java
+++ Tela.java
@@ -1,26 +1,34 @@
 
-import java.awt.*;
-import javax.swing.*;
+import java.awt.Color;
+import java.awt.Canvas;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+import java.util.ArrayList;
+
+import pecas.Peca;
+import utils.Config;
+import utils.Cor;
+import tabuleiro.SetupPecas;
+import tabuleiro.Tabuleiro;
 
 class Tela extends JFrame {
 
     // constructor
-    Tela() {
+    Tela(Tabuleiro tabuleiro, ArrayList<Peca> pecasBrancas, ArrayList<Peca> pecasPretas) {
         super("xadrez");
 
         // create a empty canvas
         Canvas canvas = new Canvas() {
 
             // paint the canvas
-            public void paint(Graphics gui) {
+            public void paint(Graphics graphics) {
                 // set color to black
-                gui.setColor(Color.black);
+                graphics.setColor(Color.black);
                 // draw a square
-                for (int i = 0; i < 8; i++) {
-                    for (int j = i % 2; j < 8; j += 2) {
-                        gui.fillRect(100 * j, 100 * i, 100, 100);
-                    }
-                }
+                tabuleiro.desenha(graphics);
             }
         };
 
@@ -28,12 +36,16 @@ class Tela extends JFrame {
         canvas.setBackground(Color.white);
 
         add(canvas);
-        setSize(900, 900);
+        setSize(Config.LARGURA_TELA, Config.ALTURA_TELA);
         setVisible(true);
     }
 
     // Main Method
     public static void main(String args[]) {
-        Tela c = new Tela();
+        Tabuleiro tabuleiro = new Tabuleiro();
+        ArrayList<Peca> pecasBrancas = SetupPecas.setup(Cor.BRANCO);
+        ArrayList<Peca> pecasPretas = SetupPecas.setup(Cor.PRETO);
+        Tela tela = new Tela(tabuleiro, pecasBrancas, pecasPretas);
+        tela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
