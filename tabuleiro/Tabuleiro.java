78e7b9573278fa3a994784ff31dfb2a156ab354a
RodolphoVSantoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2023-04-20 12:36:41 -0300

    fix: add 2d matrix of pieces

--- tabuleiro/Tabuleiro.java
+++ tabuleiro/Tabuleiro.java
@@ -7,19 +7,40 @@ import java.util.ArrayList;
 import config.Config;
 import gui.Sprite;
 import pecas.Peca;
+import utils.Posicao;
 
 public class Tabuleiro {
     private Sprite sprite;
     private ArrayList<Peca> pecasBrancas;
     private ArrayList<Peca> pecasPretas;
+    private Peca[][] posicoesPecas;
+    private Historico historico;
 
     public Tabuleiro(ArrayList<Peca> pecasBrancas, ArrayList<Peca> pecasPretas) {
         this.sprite = new Sprite(Config.IMAGEM_TABULEIRO, 0, 0);
+        this.historico = new Historico();
+
         this.pecasBrancas = pecasBrancas;
         this.pecasPretas = pecasPretas;
         this.pecasBrancas.forEach(p -> p.setTabuleiro(this));
         this.pecasPretas.forEach(p -> p.setTabuleiro(this));
+        
+        this.posicoesPecas = new Peca[8][8];
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                this.posicoesPecas[i][j] = null;
+            }
+        }
+        this.pecasBrancas.forEach(peca->{
+            Posicao posicao = peca.getPosicaoTabuleiro();
+            this.posicoesPecas[posicao.x][posicao.y] = peca;
+        });
+        this.pecasPretas.forEach(peca->{
+            Posicao posicao = peca.getPosicaoTabuleiro();
+            this.posicoesPecas[posicao.x][posicao.y] = peca;
+        });
     }
+
 
     public void desenha(Graphics graphics, ImageObserver observer) {
         this.sprite.desenha(graphics, observer);
