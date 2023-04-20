c7257e20d8e5d91efa04a0fa8dc3f97a0d71fd1c
RodolphoVSantoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2023-04-20 13:54:53 -0300

    feat: Bishop move restrictions

--- pecas/Bispo.java
+++ pecas/Bispo.java
@@ -1,6 +1,9 @@
 package pecas;
 
 import utils.Posicao;
+
+import java.util.ArrayList;
+
 import utils.Cor;
 
 public class Bispo extends Peca {
@@ -8,27 +11,61 @@ public class Bispo extends Peca {
     public Bispo(Posicao posicao, Cor cor) {
         super(posicao, cor, TipoPeca.BISPO);
     }
-
     @Override
-    protected boolean podeMover(Posicao posicao) {
-        return true;
+    public ArrayList<Posicao> getMovimentosPossiveis() {
+        ArrayList<Posicao> movimentosPossiveis = new ArrayList<Posicao>();
+        Posicao posicaoAtual = this.getPosicaoTabuleiro();
+        int j = 0;
+        for(int i = posicaoAtual.x + 1;i < 8;i++){
+            j++;
+            if(i + j < 8){
+                Posicao proximaPosicao = new Posicao(i, i + j);
+                Peca peca = this.tabuleiro.getPeca(proximaPosicao);
+                if(peca!=null) {
+                    if(this.podeCapturar(proximaPosicao)){
+                        movimentosPossiveis.add(proximaPosicao);
+                    }
+                    break;
+                }
+                movimentosPossiveis.add(proximaPosicao);
+            }
+            if(i - j >= 0){
+                Posicao proximaPosicao = new Posicao(i, i - j);
+                Peca peca = this.tabuleiro.getPeca(proximaPosicao);
+                if(peca!=null) {
+                    if(this.podeCapturar(proximaPosicao)){
+                        movimentosPossiveis.add(proximaPosicao);
+                    }
+                    break;
+                }
+                movimentosPossiveis.add(proximaPosicao);
+            }
+        }
+        for(int i = posicaoAtual.x - 1;i >= 0;i--){
+            j++;
+            if(i + j < 8){
+                Posicao proximaPosicao = new Posicao(i, i + j);
+                Peca peca = this.tabuleiro.getPeca(proximaPosicao);
+                if(peca!=null) {
+                    if(this.podeCapturar(proximaPosicao)){
+                        movimentosPossiveis.add(proximaPosicao);
+                    }
+                    break;
+                }
+                movimentosPossiveis.add(proximaPosicao);
+            }
+            if(i - j >= 0){
+                Posicao proximaPosicao = new Posicao(i, i - j);
+                Peca peca = this.tabuleiro.getPeca(proximaPosicao);
+                if(peca!=null) {
+                    if(this.podeCapturar(proximaPosicao)){
+                        movimentosPossiveis.add(proximaPosicao);
+                    }
+                    break;
+                }
+                movimentosPossiveis.add(proximaPosicao);
+            }
+        }
+        return movimentosPossiveis;
     }
-
-    @Override
-    protected boolean podeCapturar(Posicao posicao) {
-        return false;
-    }
-
-    @Override
-    protected Peca tentaCapturar(Posicao posicao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'tentaCapturar'");
-    }
-
-    @Override
-    public boolean tentaMover(Posicao posicao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'tentaMover'");
-    }
-
 }
