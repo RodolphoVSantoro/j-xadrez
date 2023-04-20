bf4d2fb5e5c94d1f5394315038775bd8aa1b3840
RodolphoVSantoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2023-04-20 13:55:02 -0300

    feat: Knight move restrictions

--- pecas/Cavalo.java
+++ pecas/Cavalo.java
@@ -1,6 +1,9 @@
 package pecas;
 
 import utils.Posicao;
+
+import java.util.ArrayList;
+
 import utils.Cor;
 
 public class Cavalo extends Peca {
@@ -8,29 +11,31 @@ public class Cavalo extends Peca {
     public Cavalo(Posicao posicao, Cor cor) {
         super(posicao, cor, TipoPeca.CAVALO);
     }
-
+    
     @Override
-    protected boolean podeMover(Posicao posicao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'podeMover'");
-    }
+    public ArrayList<Posicao> getMovimentosPossiveis() {
+        ArrayList<Posicao> movimentosPossiveis = new ArrayList<Posicao>();
+        Posicao posicaoAtual = this.getPosicaoTabuleiro();
+        Posicao proximaPosicao = new Posicao(posicaoAtual.x + 2, posicaoAtual.y + 1);
+        this.cavaloAdicionaMovimentoPossivel(movimentosPossiveis, proximaPosicao);
 
-    @Override
-    protected boolean podeCapturar(Posicao posicao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'podeCapturar'");
-    }
+        proximaPosicao = new Posicao(posicaoAtual.x + 2, posicaoAtual.y + 1);
+        this.cavaloAdicionaMovimentoPossivel(movimentosPossiveis, proximaPosicao);
 
-    @Override
-    protected Peca tentaCapturar(Posicao posicao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'tentaCapturar'");
-    }
+        proximaPosicao = new Posicao(posicaoAtual.x + 2, posicaoAtual.y + 1);
+        this.cavaloAdicionaMovimentoPossivel(movimentosPossiveis, proximaPosicao);
 
-    @Override
-    public boolean tentaMover(Posicao posicao) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'tentaMover'");
+        proximaPosicao = new Posicao(posicaoAtual.x + 2, posicaoAtual.y + 1);
+        this.cavaloAdicionaMovimentoPossivel(movimentosPossiveis, proximaPosicao);
+        
+        return movimentosPossiveis;
     }
-
+    private void cavaloAdicionaMovimentoPossivel(ArrayList<Posicao> movimentosPossiveis, Posicao proximaPosicao){
+        if(proximaPosicao.x < 8 && proximaPosicao.x >= 0 && proximaPosicao.y < 8 && proximaPosicao.y >= 0){
+            Peca peca = this.tabuleiro.getPeca(proximaPosicao);
+            if(peca == null || this.podeCapturar(proximaPosicao)){
+                movimentosPossiveis.add(proximaPosicao);
+            }
+        }
+    }
 }
