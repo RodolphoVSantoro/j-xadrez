2562e5ed06db322977742ade56a29a601eb8c4f3
RodolphoVSantoro <rodolphovs@gmail.com>
2023-04-21 21:41:54 -0300

    fix: move movimento to a new class
    Co-authored-by: K-APereira <kaiopereira@id.uff.br>

--- maquinaDeRegras/Movimento.java
+++ maquinaDeRegras/Movimento.java
@@ -3,21 +3,25 @@ package maquinaDeRegras;
 import pecas.Peca;
 import utils.Posicao;
 
-public class Movimento{
+public class Movimento {
     private Peca peca;
     private Posicao posicaoAnterior;
     private Posicao posicaoPosterior;
-    Movimento(Peca peca, Posicao posicaoAnterior, Posicao posicaoPosterior){
+
+    public Movimento(Peca peca, Posicao posicaoAnterior, Posicao posicaoPosterior) {
         this.peca = peca;
         this.posicaoAnterior = posicaoAnterior;
         this.posicaoPosterior = posicaoPosterior;
     }
+
     public Peca getPeca() {
         return peca;
     }
+
     public Posicao getPosicaoAnterior() {
         return posicaoAnterior;
     }
+
     public Posicao getPosicaoPosterior() {
         return posicaoPosterior;
     }
