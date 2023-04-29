34ecc8a07391c2a4694d06c3ad5f5aef94b60267
RodolphoVSantoro <rodolphovs@gmail.com>
2023-04-29 11:40:35 -0300

    feat: add movimento by Movimento copy

--- maquinaDeRegras/Historico.java
+++ maquinaDeRegras/Historico.java
@@ -8,30 +8,38 @@ import utils.Posicao;
 public class Historico {
     private ArrayList<Movimento> movimentos;
     private int ultimoMovimento;
-    Historico(){
+
+    Historico() {
         this.movimentos = new ArrayList<Movimento>();
         this.ultimoMovimento = -1;
     }
-    void adicionaMovimento(Peca peca, Posicao posicaoAnterior, Posicao posicaoPosterior){
-        this.movimentos.add(new Movimento(peca,posicaoAnterior,posicaoPosterior));
+
+    public void adicionaMovimento(Movimento movimento) {
+        Peca peca = movimento.getPeca();
+        Posicao posicaoAnterior = movimento.getPosicaoAnterior();
+        Posicao posicaoPosterior = movimento.getPosicaoPosterior();
+
+        this.movimentos.add(new Movimento(peca, posicaoAnterior, posicaoPosterior));
         this.ultimoMovimento++;
     }
-    Movimento getUltimoMovimento(){
+
+    public Movimento getUltimoMovimento() {
         try {
             Movimento ultimoMovimento = this.movimentos.get(this.ultimoMovimento);
             return ultimoMovimento;
-        } catch(Error error){
+        } catch (Error error) {
             System.out.println("Não existe movimento " + Integer.toString(this.ultimoMovimento));
             System.out.println(error.toString());
             return null;
         }
     }
-    Movimento reverteMovimento(){
+
+    public Movimento reverteMovimento() {
         try {
             Movimento ultimoMovimento = this.movimentos.remove(this.ultimoMovimento);
             this.ultimoMovimento--;
             return ultimoMovimento;
-        } catch(Error error){
+        } catch (Error error) {
             System.out.println("Impossivel voltar na jogada");
             System.out.println(error.toString());
             return null;
