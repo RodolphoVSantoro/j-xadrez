ed29e46e4cb97776ddc1ca90690eab34da82ee7d
RodolphoVSantoro <rodolphovs@gmail.com>
2023-04-29 15:53:59 -0300

    feat: add captured pieces to movement

--- maquinaDeRegras/Movimento.java
+++ maquinaDeRegras/Movimento.java
@@ -7,11 +7,24 @@ public class Movimento {
     private Peca peca;
     private Posicao posicaoAnterior;
     private Posicao posicaoPosterior;
+    private Peca pecaCapturada;
 
     public Movimento(Peca peca, Posicao posicaoAnterior, Posicao posicaoPosterior) {
         this.peca = peca;
         this.posicaoAnterior = posicaoAnterior;
         this.posicaoPosterior = posicaoPosterior;
+        this.pecaCapturada = null;
+    }
+
+    public Movimento(Peca peca, Posicao posicaoAnterior, Posicao posicaoPosterior, Peca pecaCapturada) {
+        this.peca = peca;
+        this.posicaoAnterior = posicaoAnterior;
+        this.posicaoPosterior = posicaoPosterior;
+        this.pecaCapturada = pecaCapturada;
+    }
+
+    public Peca getPecaCapturada() {
+        return pecaCapturada;
     }
 
     public Peca getPeca() {
