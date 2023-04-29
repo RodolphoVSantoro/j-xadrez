9ffb64219b5eca7a110bf4ba00a6518085c937e1
RodolphoVSantoro <rodolphovs@gmail.com>
2023-04-29 12:36:29 -0300

    fix: refactor bishop

--- pecas/Bispo.java
+++ pecas/Bispo.java
@@ -11,61 +11,44 @@ public class Bispo extends Peca {
     public Bispo(Posicao posicao, Cor cor) {
         super(posicao, cor, TipoPeca.BISPO);
     }
+
     @Override
     public ArrayList<Posicao> getMovimentosPossiveis() {
         ArrayList<Posicao> movimentosPossiveis = new ArrayList<Posicao>();
-        Posicao posicaoAtual = this.getPosicaoTabuleiro();
-        int j = 0;
-        for(int i = posicaoAtual.x + 1;i < 8;i++){
-            j++;
-            if(i + j < 8){
-                Posicao proximaPosicao = new Posicao(i, i + j);
-                Peca peca = this.tabuleiro.getPeca(proximaPosicao);
-                if(peca!=null) {
-                    if(this.podeCapturar(proximaPosicao)){
-                        movimentosPossiveis.add(proximaPosicao);
-                    }
-                    break;
-                }
-                movimentosPossiveis.add(proximaPosicao);
-            }
-            if(i - j >= 0){
-                Posicao proximaPosicao = new Posicao(i, i - j);
-                Peca peca = this.tabuleiro.getPeca(proximaPosicao);
-                if(peca!=null) {
-                    if(this.podeCapturar(proximaPosicao)){
-                        movimentosPossiveis.add(proximaPosicao);
-                    }
-                    break;
-                }
-                movimentosPossiveis.add(proximaPosicao);
-            }
-        }
-        for(int i = posicaoAtual.x - 1;i >= 0;i--){
-            j++;
-            if(i + j < 8){
-                Posicao proximaPosicao = new Posicao(i, i + j);
-                Peca peca = this.tabuleiro.getPeca(proximaPosicao);
-                if(peca!=null) {
-                    if(this.podeCapturar(proximaPosicao)){
-                        movimentosPossiveis.add(proximaPosicao);
-                    }
-                    break;
-                }
-                movimentosPossiveis.add(proximaPosicao);
-            }
-            if(i - j >= 0){
-                Posicao proximaPosicao = new Posicao(i, i - j);
-                Peca peca = this.tabuleiro.getPeca(proximaPosicao);
-                if(peca!=null) {
-                    if(this.podeCapturar(proximaPosicao)){
-                        movimentosPossiveis.add(proximaPosicao);
-                    }
-                    break;
-                }
-                movimentosPossiveis.add(proximaPosicao);
-            }
-        }
+
+        this.addMovimentoDiagonal(movimentosPossiveis, posicaoTabuleiro, 1, 1);
+        this.addMovimentoDiagonal(movimentosPossiveis, posicaoTabuleiro, 1, -1);
+        this.addMovimentoDiagonal(movimentosPossiveis, posicaoTabuleiro, -1, 1);
+        this.addMovimentoDiagonal(movimentosPossiveis, posicaoTabuleiro, -1, -1);
         return movimentosPossiveis;
+    }
+
+    /**
+     * Só chamar se já validou que não tem
+     * peça no caminho
+     */
+    private boolean addPosicaoValida(Posicao proximaPosicao, ArrayList<Posicao> movimentosPossiveis) {
+        Peca peca = this.tabuleiro.getPeca(proximaPosicao);
+        if (peca != null) {
+            if (this.podeCapturar(proximaPosicao)) {
+                movimentosPossiveis.add(proximaPosicao);
+            }
+            return false;
+        }
+        movimentosPossiveis.add(proximaPosicao);
+        return true;
+    }
+
+    private void addMovimentoDiagonal(ArrayList<Posicao> movimentosPossiveis, Posicao posicaoAtual, int direcaoX,
+            int direcaoY) {
+        boolean podeContinuar = true;
+        int i = posicaoAtual.x + direcaoX;
+        int j = posicaoAtual.y + direcaoY;
+        while (i < 8 && i >= 0 && j < 8 && j >= 0 && podeContinuar) {
+            Posicao proximaPosicao = new Posicao(i, j);
+            podeContinuar = this.addPosicaoValida(proximaPosicao, movimentosPossiveis);
+            i += direcaoX;
+            j += direcaoY;
+        }
     }
 }
