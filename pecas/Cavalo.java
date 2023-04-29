c84784fdbfda2adca50a2e08db1213300554b507
RodolphoVSantoro <rodolphovs@gmail.com>
2023-04-29 12:36:41 -0300

    fix: refactor knight

--- pecas/Cavalo.java
+++ pecas/Cavalo.java
@@ -11,29 +11,35 @@ public class Cavalo extends Peca {
     public Cavalo(Posicao posicao, Cor cor) {
         super(posicao, cor, TipoPeca.CAVALO);
     }
-    
+
+    private static final int[][] MOVIMENTOS_POSSIVEIS = {
+            { 2, 1 },
+            { 2, -1 },
+            { -2, -1 },
+            { -2, 1 },
+            { -1, 2 },
+            { 1, 2 },
+            { 1, -2 },
+            { -1, -2 }
+    };
+
     @Override
     public ArrayList<Posicao> getMovimentosPossiveis() {
         ArrayList<Posicao> movimentosPossiveis = new ArrayList<Posicao>();
         Posicao posicaoAtual = this.getPosicaoTabuleiro();
-        Posicao proximaPosicao = new Posicao(posicaoAtual.x + 2, posicaoAtual.y + 1);
-        this.cavaloAdicionaMovimentoPossivel(movimentosPossiveis, proximaPosicao);
 
-        proximaPosicao = new Posicao(posicaoAtual.x + 2, posicaoAtual.y + 1);
-        this.cavaloAdicionaMovimentoPossivel(movimentosPossiveis, proximaPosicao);
+        for (int[] movimento : MOVIMENTOS_POSSIVEIS) {
+            Posicao proximaPosicao = new Posicao(posicaoAtual.x + movimento[0], posicaoAtual.y + movimento[1]);
+            this.cavaloAdicionaMovimentoPossivel(movimentosPossiveis, proximaPosicao);
+        }
 
-        proximaPosicao = new Posicao(posicaoAtual.x + 2, posicaoAtual.y + 1);
-        this.cavaloAdicionaMovimentoPossivel(movimentosPossiveis, proximaPosicao);
-
-        proximaPosicao = new Posicao(posicaoAtual.x + 2, posicaoAtual.y + 1);
-        this.cavaloAdicionaMovimentoPossivel(movimentosPossiveis, proximaPosicao);
-        
         return movimentosPossiveis;
     }
-    private void cavaloAdicionaMovimentoPossivel(ArrayList<Posicao> movimentosPossiveis, Posicao proximaPosicao){
-        if(proximaPosicao.x < 8 && proximaPosicao.x >= 0 && proximaPosicao.y < 8 && proximaPosicao.y >= 0){
+
+    private void cavaloAdicionaMovimentoPossivel(ArrayList<Posicao> movimentosPossiveis, Posicao proximaPosicao) {
+        if (proximaPosicao.x < 8 && proximaPosicao.x >= 0 && proximaPosicao.y < 8 && proximaPosicao.y >= 0) {
             Peca peca = this.tabuleiro.getPeca(proximaPosicao);
-            if(peca == null || this.podeCapturar(proximaPosicao)){
+            if (peca == null || this.podeCapturar(proximaPosicao)) {
                 movimentosPossiveis.add(proximaPosicao);
             }
         }
