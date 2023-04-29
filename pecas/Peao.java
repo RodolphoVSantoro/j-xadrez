ddc7a07c6d22cd611a1eaa64e0667089f183b583
RodolphoVSantoro <rodolphovs@gmail.com>
2023-04-29 12:37:29 -0300

    fix: pawn move dir

--- pecas/Peao.java
+++ pecas/Peao.java
@@ -9,36 +9,41 @@ import utils.Cor;
 public class Peao extends Peca {
 
     private boolean primeiroMovimento = true;
+
     public Peao(Posicao posicao, Cor cor) {
         super(posicao, cor, TipoPeca.PEAO);
     }
+
     @Override
     public ArrayList<Posicao> getMovimentosPossiveis() {
-        if(this.getCor() == Cor.PRETO) {
+        if (this.getCor() == Cor.PRETO) {
             return this.getMovimentosPossiveisPeao(1);
         }
         return this.getMovimentosPossiveisPeao(-1);
     }
+
     private ArrayList<Posicao> getMovimentosPossiveisPeao(int dir) {
         ArrayList<Posicao> movimentosPossiveis = new ArrayList<Posicao>();
         Posicao posicaoAtual = this.getPosicaoTabuleiro();
         Posicao proximaPosicao = new Posicao(posicaoAtual.x, posicaoAtual.y + 1 * dir);
         Peca peca = this.tabuleiro.getPeca(proximaPosicao);
-        if(peca == null) {
+        if (peca == null) {
             movimentosPossiveis.add(proximaPosicao);
         }
-        if(this.primeiroMovimento) {
+        if (this.primeiroMovimento) {
             proximaPosicao = new Posicao(posicaoAtual.x, posicaoAtual.y + 2 * dir);
             peca = this.tabuleiro.getPeca(proximaPosicao);
-            if(peca == null) {
+            if (peca == null) {
                 movimentosPossiveis.add(proximaPosicao);
             }
         }
-        if(this.podeCapturar(new Posicao(posicaoAtual.x + 1, posicaoAtual.y - 1))) {
-            movimentosPossiveis.add(new Posicao(posicaoAtual.x + 1, posicaoAtual.y + 1 * dir));
+        Posicao posicaoEsquerda = new Posicao(posicaoAtual.x + 1, posicaoAtual.y + 1 * dir);
+        if (this.podeCapturar(posicaoEsquerda)) {
+            movimentosPossiveis.add(posicaoEsquerda);
         }
-        if(this.podeCapturar(new Posicao(posicaoAtual.x - 1, posicaoAtual.y + 1))) {
-            movimentosPossiveis.add(new Posicao(posicaoAtual.x - 1, posicaoAtual.y + 1 * dir));
+        Posicao posicaoDireita = new Posicao(posicaoAtual.x - 1, posicaoAtual.y + 1 * dir);
+        if (this.podeCapturar(posicaoDireita)) {
+            movimentosPossiveis.add(posicaoDireita);
         }
         return movimentosPossiveis;
     }
