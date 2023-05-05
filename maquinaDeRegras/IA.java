41897251ad5bf0ee874e4e6dd9ac6f07f6dfdd65
K-APereira <kaiopereira@id.uff.br>
2023-05-05 00:24:28 -0300

    fix: melhoria da IA

--- maquinaDeRegras/IA.java
+++ maquinaDeRegras/IA.java
@@ -52,7 +52,7 @@ public class IA {
         double betatemp=beta;
         boolean para=false;
         if (profundidade <= 0 || this.maquinaDeRegras.chegouFimDeJogo()) {
-            return new Movimento(null, null, null, this.getValorTabuleiro()* Math.random() / 10.0 + 1.0);
+            return new Movimento(null, null, null, this.getValorTabuleiro());
         }
         Movimento melhorMovimento=new Movimento(null, null, null, Integer.MIN_VALUE);
         ArrayList<Peca> pecas = this.tabuleiro.getPecas(this.cor);
@@ -65,7 +65,7 @@ public class IA {
                 
                 novoMovimento.setValor(this.minMax(profundidade - 1,alphatemp,betatemp,false).getValor());
 
-                if(novoMovimento.getValor()>=melhorMovimento.getValor())melhorMovimento=novoMovimento;
+                if(novoMovimento.getValor() > melhorMovimento.getValor() || ( novoMovimento.getValor() == melhorMovimento.getValor() && Math.random() > 0.5 ))melhorMovimento=novoMovimento;
 
                 this.maquinaDeRegras.desfazUltimoMovimento();
 
@@ -101,7 +101,7 @@ public class IA {
                     // throw new RuntimeException("Movimento inválido computando minMax "+novoMovimento.getPosicaoAnterior().x+" "+novoMovimento.getPosicaoAnterior().y+" "+novoMovimento.getPosicaoPosterior().x+" "+novoMovimento.getPosicaoPosterior().y);
                 
                 novoMovimento.setValor(this.minMax(profundidade - 1,alphatemp,betatemp,true).getValor());
-                if(novoMovimento.getValor()<=melhorMovimento.getValor())melhorMovimento=novoMovimento;
+                if(novoMovimento.getValor() < melhorMovimento.getValor() || ( novoMovimento.getValor() == melhorMovimento.getValor() && Math.random() > 0.5 ))melhorMovimento=novoMovimento;
                 this.maquinaDeRegras.desfazUltimoMovimento();
                 if(novoMovimento.getValor()<=betatemp)betatemp=novoMovimento.getValor();
                 if(betatemp<=alphatemp){
